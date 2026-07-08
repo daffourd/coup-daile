9574ad9416b225cad812e72367cce8a6d98e2fe6
Yakari <daffourd@gmail.com>
2026-07-08 16:49:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker Coup d'Aile, Lot 1
 // Cache le coeur vital pour un fonctionnement hors-ligne.
-const CACHE = 'coup-daile-v3';
+const CACHE = 'coup-daile-v4';
 const ASSETS = [
   './',
   './index.html',
