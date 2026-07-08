3bbe5f521a315f13b8c819c51886e48e68254eff
Yakari <daffourd@gmail.com>
2026-07-08 18:29:39 +0200

    Especes: liste etendue a 100 oiseaux courants de France, cache sw v7

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker Coup d'Aile, Lot 1
 // Cache le coeur vital pour un fonctionnement hors-ligne.
-const CACHE = 'coup-daile-v6';
+const CACHE = 'coup-daile-v7';
 const ASSETS = [
   './',
   './index.html',
