8f44924ae9353bfe1f9528186b14ea8db27abab7
Yakari <daffourd@gmail.com>
2026-07-09 09:44:42 +0200

    Photos especes, avatar, niveaux et badges, connexion code OTP, sw v11

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker Coup d'Aile, Lot 1
 // Cache le coeur vital pour un fonctionnement hors-ligne.
-const CACHE = 'coup-daile-v10';
+const CACHE = 'coup-daile-v11';
 const ASSETS = [
   './',
   './index.html',
