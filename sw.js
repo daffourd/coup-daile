c1b8790a25de7045d9bdda6071095384d5e31549
Yakari <daffourd@gmail.com>
2026-07-09 20:19:47 +0200

    Admin: liste des membres inscrits + moderation + dedoublonnage, sw v16

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker Coup d'Aile, Lot 1
 // Cache le coeur vital pour un fonctionnement hors-ligne.
-const CACHE = 'coup-daile-v15';
+const CACHE = 'coup-daile-v16';
 const ASSETS = [
   './',
   './index.html',
