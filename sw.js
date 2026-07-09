aa25b056beae52c4cffc4b86a682b17e77691b50
Yakari <daffourd@gmail.com>
2026-07-09 22:28:00 +0200

    Carnet: modifier, supprimer et horodater les observations (SW v18)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker Coup d'Aile, Lot 1
 // Cache le coeur vital pour un fonctionnement hors-ligne.
-const CACHE = 'coup-daile-v17';
+const CACHE = 'coup-daile-v18';
 const ASSETS = [
   './',
   './index.html',
